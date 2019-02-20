467131da40ebea06da534f0c01016e0d944ad93b
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-02-20 11:46:31 +0100

    javascripting

---
+++ for-loop.js
@@ -0,0 +1,4 @@
+let limit = 10;
+for (let total = 0; total < limit; total++) {
+console.log (total);
+}
---
+++ rounding-numbers.js
@@ -0,0 +1,5 @@
+let roundUp = 1.5;
+Math.round (0.5);
+let rounded = Math.round (roundUp);
+
+console.log (rounded);
